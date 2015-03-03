fbceb0d66215ecc35749ece2b0365d34666cbebb
Sebastien Andary <sebastien@naturalpad.fr>
2015-03-03 23:17:31 +0100

    player moving and orienting

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
-
+Player = require("Player")
+require("aphrodisiacs/utils/vector")
 
 function love.load(arg)
-
+	player = Player.new()
 end
 
 function love.keypressed(key, isrepeat)
@@ -10,12 +11,34 @@ function love.keypressed(key, isrepeat)
 	end
 end
 
-function 
-
 function love.update(dt)
+	inputs = { 
+		dir = Vector.new(),
+		target = Vector.new(love.mouse.getPosition()) 
+	}
 
+	if love.keyboard.isDown('q') then
+		inputs.dir = inputs.dir + Vector.left
+	end
+
+	if love.keyboard.isDown('d') then
+		inputs.dir = inputs.dir + Vector.right
+	end
+	
+	if love.keyboard.isDown('z') then
+		inputs.dir = inputs.dir + Vector.up
+	end
+	
+	if love.keyboard.isDown('s') then
+		inputs.dir = inputs.dir + Vector.down
+	end
+	
+	if (inputs.dir.x > 0 or inputs.dir.y > 0) then 
+		inputs.dir = inputs.dir:normalized()
+	end
+	player:update(dt, inputs)
 end
 
 function love.draw()
-
+	player:draw()
 end
--- player.lua
+++ player.lua
@@ -1,31 +1,47 @@
 local Player = {}
 Player.__index = Player
 
-local rnd = love.math.random()
+local rnd = love.math.random
 local maxColorRnd = 64
 
 require("aphrodisiacs/utils/vector")
-	
+require("aphrodisiacs/utils/mathExtension")
+
 function Player.new()
-	local new = { pos = Vector.new }
+	local new = {
+		pos = Vector.new(),
+		vel = 100
+	}
 	new.bodyColor = { 127 + rnd(0, maxColorRnd), 64 + rnd(0, maxColorRnd), 64 + rnd(0, maxColorRnd) }
 	new.headColor = { 127 + rnd(0, maxColorRnd), 64 + rnd(0, maxColorRnd), 64 + rnd(0, maxColorRnd) }
 	return setmetatable(new, Player)
 end
 
 function Player:draw()
-	local height, width = 10, 20
+	local height, width = 20, 40
+
+	love.graphics.push()
+	love.graphics.translate(self.pos.x, self.pos.y)
+	love.graphics.rotate(self.rot)
+	
 	-- body
 	love.graphics.setColor(self.bodyColor)
 	love.graphics.rectangle('fill', -width / 2, -height / 2, width, height)
 	-- head
 	love.graphics.setColor(self.headColor)
-	love.graphics.circle('fill', 0, 0, 6, 12)
+	love.graphics.circle('fill', 0, 0, 11, 10)
+
+	love.graphics.pop()
 end
 
 function Player:update(dt, inputs)
 	if inputs.dir then
-		self.x, self.y = 
+		local targetPos = self.pos + inputs.dir * self.vel * dt
+		self.pos = targetPos
+	end
+	if inputs.target then
+		local targetRot = (inputs.target - self.pos):angle() + math.pi * 0.5
+		self.rot = targetRot
 	end
 end
 
