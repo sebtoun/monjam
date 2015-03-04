f988ba010722a0b1e82a506b3bd01c39ce8c4161
Sebastien Andary <sebastien@naturalpad.fr>
2015-03-04 20:35:53 +0100

    adds background and camera

--- main.lua
+++ main.lua
@@ -1,8 +1,20 @@
 Player = require("Player")
 require("aphrodisiacs/utils/vector")
 
+Camera = require "camera"
+
+local rnd = love.math.random
+local maxColorRnd = 180
+
 function love.load(arg)
 	player = Player.new()
+	cam = Camera(0, 0)
+	floorImg = love.graphics.newImage("assets/grid.jpg")
+	-- floorColor = { rnd(0, maxColorRnd), rnd(0, maxColorRnd), rnd(0, maxColorRnd) }
+	floorColor = { 200, 200, 200 }
+
+	love.mouse.setVisible(false)
+	love.mouse.setGrabbed(true)
 end
 
 function love.keypressed(key, isrepeat)
@@ -14,7 +26,7 @@ end
 function love.update(dt)
 	inputs = { 
 		dir = Vector.new(),
-		target = Vector.new(love.mouse.getPosition()) 
+		target = Vector.new(cam:mousepos()) 
 	}
 
 	if love.keyboard.isDown('q') then
@@ -36,9 +48,27 @@ function love.update(dt)
 	if (inputs.dir.x > 0 or inputs.dir.y > 0) then 
 		inputs.dir = inputs.dir:normalized()
 	end
+
 	player:update(dt, inputs)
+
+	local targetx, targety = (player.pos.x * 2 + inputs.target.x) / 3, (player.pos.y * 2 + inputs.target.y) / 3
+	-- local dx, dy = targetx - cam.x, targety - cam.y
+    -- cam:move(dx * 5 * dt, dy * 5 * dt)
+    cam:lookAt(math.round(targetx), math.round(targety))
 end
 
 function love.draw()
+	local w,h = floorImg:getWidth(), floorImg:getHeight()
+	local xmin, ymin = cam:worldCoords(0, 0)
+	local xmax, ymax = cam:worldCoords(love.graphics.getWidth(), love.graphics.getHeight())
+	cam:attach()
+	love.graphics.setColor(floorColor)
+	for i = math.floor(xmin / w), math.floor(xmax / w) do
+		for j = math.floor(ymin / h), math.floor(ymax / h) do
+			love.graphics.draw(floorImg, i * w, j * h)
+		end
+	end
 	player:draw()
+	cam:detach()
+	love.graphics.print("Current FPS: "..tostring(love.timer.getFPS()), 10, 10)
 end
--- player.lua
+++ player.lua
@@ -2,7 +2,7 @@ local Player = {}
 Player.__index = Player
 
 local rnd = love.math.random
-local maxColorRnd = 64
+local maxColorRnd = 180
 
 require("aphrodisiacs/utils/vector")
 require("aphrodisiacs/utils/mathExtension")
@@ -10,10 +10,17 @@ require("aphrodisiacs/utils/mathExtension")
 function Player.new()
 	local new = {
 		pos = Vector.new(),
-		vel = 100
+		maxSpeed = 200,
+		vel = Vector.new(),
+		acc = Vector.new(),
+		rot = 0,
+		angularVel = 0
 	}
-	new.bodyColor = { 127 + rnd(0, maxColorRnd), 64 + rnd(0, maxColorRnd), 64 + rnd(0, maxColorRnd) }
-	new.headColor = { 127 + rnd(0, maxColorRnd), 64 + rnd(0, maxColorRnd), 64 + rnd(0, maxColorRnd) }
+	-- new.bodyColor = { rnd(0, maxColorRnd), rnd(0, maxColorRnd), rnd(0, maxColorRnd) }
+	new.bodyColor = { 160, 40, 40 }
+	-- new.headColor = { rnd(0, maxColorRnd), rnd(0, maxColorRnd), rnd(0, maxColorRnd) }
+	new.headColor = { 239, 208, 207 }
+
 	return setmetatable(new, Player)
 end
 
@@ -36,11 +43,18 @@ end
 
 function Player:update(dt, inputs)
 	if inputs.dir then
-		local targetPos = self.pos + inputs.dir * self.vel * dt
-		self.pos = targetPos
+		-- smooth velocity
+		local targetVel = inputs.dir * self.maxSpeed
+		-- self.vel.x, self.acc.x = math.smoothDamp(self.vel.x, targetVel.x, self.acc.x, 0.2, math.huge, dt) 
+		-- self.vel.y, self.acc.y = math.smoothDamp(self.vel.y, targetVel.y, self.acc.y, 0.2, math.huge, dt)
+		-- move position
+		-- self.pos = self.pos + self.vel * dt
+		self.pos = self.pos + targetVel * dt
 	end
 	if inputs.target then
+		-- smooth rotation
 		local targetRot = (inputs.target - self.pos):angle() + math.pi * 0.5
+		-- self.rot, self.angularVel = math.smoothDampAngle(self.rot, targetRot, self.angularVel, 0.2, math.huge, dt) 
 		self.rot = targetRot
 	end
 end
