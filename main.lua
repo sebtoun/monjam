040fcb3c2632f05f7dccb04beb2e7b67f19de0e9
Sebastien Andary <sebastien@naturalpad.fr>
2015-03-09 23:53:13 +0100

    cleanup code and add comments

--- main.lua
+++ main.lua
@@ -4,18 +4,27 @@ require "aphrodisiacs/utils/vector"
 Camera = require "camera"
 Cursor = require "cursor"
 
-local rnd = love.math.random
-local maxColorRnd = 180
-local player, cam, floorImg, floorColor, cursor, camDistance
-local intent = { left = {}, right = {} }
+local floorColor = { 200, 200, 200 }
+local player, cam, floorImg, cursor, camDistance, intent
+
+function resetIntent()
+    intent = {
+        left = {
+            pressed = false,
+            released = false
+        },
+        right = {
+            pressed = false,
+            released = false
+        }
+    }
+end
 
 function love.load( arg )
     player = Player.new()
     cam = Camera( 0, 0 )
     floorImg = love.graphics.newImage( "assets/grid.jpg" )
-    -- floorColor = { rnd(0, maxColorRnd), rnd(0, maxColorRnd), rnd(0, maxColorRnd) }
-    floorColor = { 200, 200, 200 }
-
+    
     cursor = Cursor.new( player, cam )
 
     -- setup mouse mode
@@ -24,6 +33,7 @@ function love.load( arg )
     love.mouse.setRelativeMode( true )
 
     camDistance = math.floor( 0.1 * math.min( love.graphics.getWidth(), love.graphics.getHeight() ) )
+    resetIntent()
 end
 
 function love.keypressed( key, isrepeat )
@@ -53,6 +63,7 @@ function love.mousereleased( x, y, button )
 end
 
 function love.update( dt )
+    -- handle inputs
     intent.dir = Vector.new()
     intent.target = cursor:getPos():clone()
     
@@ -79,46 +90,40 @@ function love.update( dt )
     intent.left.down = love.mouse.isDown('l')
     intent.right.down = love.mouse.isDown('r')
 
+    -- update components
     player:update( dt, intent )
     cursor:update( dt )
 
+    -- handle camera
     local sight = cursor.localPos:normalized()
     local targetx, targety = (player.pos + sight * camDistance):unpack()
     local dx, dy = targetx - cam.x, targety - cam.y
     cam:move(dx * 5 * dt, dy * 5 * dt)
 
-    -- print( "cam: " .. "(" .. tostring(cam.x) .. ", " .. tostring(cam.y) .. ")" .. " cursor: " .. tostring(cursor.localPos) )
-    -- reset intent for next frame
-    intent = {
-        left = {
-            pressed = false,
-            released = false
-        },
-        right = {
-            pressed = false,
-            released = false
-        }
-    }
+    -- reset inputs for next frame
 end
 
 function love.draw()
-    local w,h = floorImg:getWidth(), floorImg:getHeight()
-    local xmin, ymin = cam:worldCoords(0, 0)
-    local xmax, ymax = cam:worldCoords(love.graphics.getWidth(), love.graphics.getHeight())
-    
     cam:attach()
     
+    -- draw floor 
+    local xmin, ymin = cam:worldCoords(0, 0)
+    local xmax, ymax = cam:worldCoords(love.graphics.getWidth(), love.graphics.getHeight())
+    local w,h = floorImg:getWidth(), floorImg:getHeight()
     love.graphics.setColor(floorColor)
     for i = math.floor(xmin / w), math.floor(xmax / w) do
         for j = math.floor(ymin / h), math.floor(ymax / h) do
             love.graphics.draw(floorImg, i * w, j * h)
         end
     end
+
+    -- draw components
     player:draw()
     cursor:draw()
 
     cam:detach()
 
+    -- draw HUD
     love.graphics.setColor(220, 220, 220 )
     love.graphics.print("Current FPS: "..tostring(love.timer.getFPS()), 10, 10)
 end
